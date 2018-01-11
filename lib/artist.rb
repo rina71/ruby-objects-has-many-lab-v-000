828583149d3b9fbf74f2611492c58cbaa68e8a3a
Yurina Shin <yurinas@gmail.com>
2018-01-11 23:16:51 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @songs = []
+    @name = name
+  end
+
+  def add_song(song)
+    # Song.new(song)
+    @songs << song
+    song.artist = self
+
+  end
+
+  def add_song_by_name(name)
+
+  end
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+    # @artist = artist
+    self.artist = Artist
+  end
+
+
+end
