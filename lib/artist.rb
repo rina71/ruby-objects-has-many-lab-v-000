ac36b6c096a5453ec72ff166221e79e645ead518
Yurina Shin <yurinas@gmail.com>
2018-01-12 00:08:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,7 @@ class Artist
   def add_song(song)
     # Song.new(song)
     @songs << song
+    @@song_count += 1
     song.artist = self
   end
 
@@ -23,7 +24,7 @@ class Artist
   end
 
   def self.song_count
-    @@song_count += 1
+
     @@song_count
   end
 
