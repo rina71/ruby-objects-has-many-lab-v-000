86f4186950423e3779a35379ea82e7285b7f56a8
Yurina Shin <yurinas@gmail.com>
2018-01-12 00:33:23 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+
+  end
+
+  def add_post(post)
+    @posts << post
+    self.post = self
+
+  end
+
+
+end
