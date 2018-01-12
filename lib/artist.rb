619a8dcf28794930375d921b25036b145725a492
Yurina Shin <yurinas@gmail.com>
2018-01-12 00:12:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name,
   @@song_count = 0
   def initialize(name)
     @songs = []
@@ -11,8 +11,8 @@ class Artist
   def add_song(song)
     # Song.new(song)
     @songs << song
-    @@song_count += 1
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(song)
